f9ce8e5fa22874e2f711c7231a87081c3cdc1a14
Olga Vorozheykina <solariskind@gmail.com>
2019-11-16 10:28:08 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
