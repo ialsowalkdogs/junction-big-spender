7c5675063b6defcc0f9dca47dba59140aaecb7ba
Artem Astashenkov <astashenkovaa@gmail.com>
2019-11-16 18:48:15 +0200

    Data model and EF dependencies

---
+++ BigSpender/BigSpender/Model/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigSpender.Model
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public decimal Value { get; set; }
+        public User User { get; set; }
+        public Merchant Merchant { get; set; }
+    }
+}
---
+++ BigSpender/BigSpender/Model/UserLivingCosts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigSpender.Model
+{
+    public class UserLivingCosts
+    {
+        public long Id { get; set; }
+        public User User { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+        public decimal Value { get; set; }
+    }
+}
