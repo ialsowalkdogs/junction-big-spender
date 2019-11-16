39ce4100134aa5b961f2deb77362182e4d2ad9ac
Olga Vorozheykina <solariskind@gmail.com>
2019-11-16 12:29:26 +0200

    Remove App.css

--- BigSpender/BigSpender/ClientApp/src/App.tsx
+++ BigSpender/BigSpender/ClientApp/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Priorities from './components/Priorities';
+import Setup from './components/Setup';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Setup />
       <Priorities />
     </div>
   );
